f9d55b6a4370d9a286045a811e0fd6cee1769177
carloseduardoamorim <caduamorim13@gmail.com>
2021-06-20 15:03:37 -0300

    Primeira propriedade MDC

---
+++ src/main/java/com/mycompany/intro/junit5/MathUtil.java
@@ -0,0 +1,10 @@
+package com.mycompany.intro.junit5;
+
+public class MathUtil {
+    public static int mdc(int a, int b){
+        if(b > 0 && a % b ==0){
+            return b;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/com/mycompany/intro/junit5/MathUtilTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.intro.junit5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilTest {
+
+    @Test
+    void testMdcAParP1() {
+        final int a = 6;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcAImparP1(){
+        final int a = 9;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+}
